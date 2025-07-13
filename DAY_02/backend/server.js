3e29526b78e558b43278655eeafd3f09e22f59ef
Arnab Jana <arnab003jn.369@gmail.com>
2025-07-13 10:27:33 +0530

    2nd one

--- DAY_02/backend/server.js
+++ DAY_02/backend/server.js
@@ -1,45 +1,45 @@
 // const express = require('express')
-import express from 'express';
+import express from "express";
 
 const app = express();
 
-app.get('/', (req,res) =>{
-   res.send("My Server is Running...");
+app.get("/", (req, res) => {
+  res.send("My Server is Running...");
 });
 
-app.get('/jokes',(req,res)=>{
-   const jokes = [
-      {
-         id: 1,
-         title: '1st one',
-         content: 'This is the first joke.'
-      },
-      {
-         id: 2,
-         title: '2nd one',
-         content: 'This is the second joke.'
-      },
-      {
-         id: 3,
-         title: '3rd one',
-         content: 'This is the third joke.'
-      },
-      {
-         id: 4,
-         title: '4th one',
-         content: 'This is the fourth joke.'
-      },
-      {
-         id: 5,
-         title: '5th one',
-         content: 'This is the fifth joke.'
-      }
-   ];
-   res.send(jokes);
+app.get("/api/jokes", (req, res) => {
+  const jokes = [
+    {
+      id: 1,
+      title: "1st one",
+      content: "This is the first joke.",
+    },
+    {
+      id: 2,
+      title: "2nd one",
+      content: "This is the second joke.",
+    },
+    {
+      id: 3,
+      title: "3rd one",
+      content: "This is the third joke.",
+    },
+    {
+      id: 4,
+      title: "4th one",
+      content: "This is the fourth joke.",
+    },
+    {
+      id: 5,
+      title: "5th one",
+      content: "This is the fifth joke.",
+    },
+  ];
+  res.send(jokes);
 });
 
 const port = process.env.PORT || 3000;
 
 app.listen(port, () => {
-   console.log(`Server is running on port http://localhost:${port}`);
+  console.log(`Server is running on port http://localhost:${port}`);
 });
